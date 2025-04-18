407a3faf47f4a84dfd6d0479f18a6a8b11e6519b
aakash2330 <aakash2330@gmail.com>
2025-04-19 03:50:51 +0530

    checkpoint

--- src/app/Components/Sections/Hero/Main.tsx
+++ src/app/Components/Sections/Hero/Main.tsx
@@ -1,9 +1,11 @@
 import { HeroTextMain } from "./HeroText";
+import { LogoTicker } from "./MarqueeText";
 
 export function HeroSection() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen bg-hero-main flex flex-col gap-12 justify-center items-center">
       <HeroTextMain></HeroTextMain>
+      <LogoTicker></LogoTicker>
     </div>
   );
 }
---
+++ src/app/Components/Sections/Hero/MarqueeText.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image, { StaticImageData } from "next/image";
+import acmeLogo from "../../../../../public/acme.svg";
+import apexLogo from "../../../../../public/apex.svg";
+import celestiaLogo from "../../../../../public/celestia.svg";
+import echoLogo from "../../../../../public/echo.svg";
+import pulseLogo from "../../../../../public/pulse.svg";
+import quantumLogo from "../../../../../public/quantum.svg";
+import { useMediaQuery } from "@/app/Hooks/useMediaQuery";
+
+export const logosTickers: LogoTicker[] = [
+  {
+    id: "1",
+    src: acmeLogo,
+    alt: "Acme Logo",
+  },
+  {
+    id: "2",
+    src: quantumLogo,
+    alt: "Quantum Logo",
+  },
+  {
+    id: "3",
+    src: echoLogo,
+    alt: "Echo Logo",
+  },
+  {
+    id: "4",
+    src: celestiaLogo,
+    alt: "Celestia Logo",
+  },
+  {
+    id: "5",
+    src: pulseLogo,
+    alt: "Pulse Logo",
+  },
+];
+
+type LogoTicker = { id: string; src: StaticImageData; alt: string };
+
+export function LogoTicker() {
+  const { isMobile } = useMediaQuery();
+
+  return (
+    <div className="py-24 text-white">
+      <div className="container">
+        <h2 className="text-center text-xl text-white/50">
+          Trusted by the world's most innovative teams
+        </h2>
+        <div className="relative mt-9 flex overflow-hidden">
+          <motion.div
+            transition={{
+              duration: 20,
+              ease: "linear",
+              repeat: Infinity,
+            }}
+            className="grid grid-cols-2 items-start justify-items-center bg-transparent gap-x-4 gap-y-4 sm:flex sm:flex-none sm:justify-center sm:gap-16 sm:pr-16"
+            initial={isMobile ? {} : { translateX: 0 }}
+            animate={isMobile ? {} : { translateX: "-50%" }}
+          >
+            {Array.from({ length: 2 }).map(() =>
+              logosTickers.map((logo) => (
+                <Image
+                  key={logo.id}
+                  src={logo.src}
+                  alt={logo.alt}
+                  className="h-12 w-auto flex-none"
+                />
+              )),
+            )}
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+}
