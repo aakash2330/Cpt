b9446ceb1b6cd2943a2ddb9584e579fbdc6d9600
aakash2330 <aakash2330@gmail.com>
2025-05-07 18:12:38 +0530

    checkpoint

---
+++ src/app/Components/Sections/HeroImage/index.tsx
@@ -0,0 +1,7 @@
+export function HeroImageSection() {
+  return (
+    <div className="h-screen bg-background w-full flex flex-col gap-12 justify-center items-center">
+      asdad
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { HeroSection } from "./Components/Sections/Hero/Main";
+import { HeroSection } from "./Components/Sections/Hero";
 import { LogoTicker } from "./Components/Sections/Hero/MarqueeText";
+import { HeroImageSection } from "./Components/Sections/HeroImage";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center pt-20">
       <HeroSection></HeroSection>
+      <HeroImageSection></HeroImageSection>
     </div>
   );
 }
