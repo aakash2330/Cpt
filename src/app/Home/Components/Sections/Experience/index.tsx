cf7f8f885c8c3a072f7f5af2b3165e73f0987c32
aakash2330 <aakash2330@gmail.com>
2025-05-10 20:55:19 +0530

    checkpoint

--- src/app/Home/Components/Sections/Experience/index.tsx
+++ src/app/Home/Components/Sections/Experience/index.tsx
@@ -1,14 +1,19 @@
+import { GlobeDemo } from "./globe";
+
 export default function ExperienceSection() {
   return (
-    <div className="h-screen bg-background w-full flex flex-col gap-12 justify-center items-center">
-      <div>we provide you the best experience</div>
-      <div>
-        Trust one of the most experienced general contracting teams for your
-        next project. City Professional Trades has been helping homes and
-        businesses in Toronto, Ontario, and surrounding areas plan, organize,
-        and execute seamless construction and remodeling projects for more than
-        a decade.
+    <div className=" mt-32 bg-background flex flex-col gap-12 justify-center items-center">
+      <div className="text-3xl font-semibold text-center">
+        WE PROVIDE YOU <br /> THE BEST EXPERIENCE
       </div>
+      <div className="text-center font-semibold">
+        Trust one of the most experienced general contracting teams for your
+        next project. City Professional Trades has been helping <br /> homes and
+        businesses in Toronto, Ontario, and surrounding areas plan, organize,
+        and execute seamless construction and <br /> remodeling projects for
+        more than a decade.
+      </div>
+      <GlobeDemo></GlobeDemo>
     </div>
   );
 }
--- src/app/Home/Components/Sections/Hero/index.tsx
+++ src/app/Home/Components/Sections/Hero/index.tsx
@@ -3,7 +3,7 @@ import { LogoTicker } from "./MarqueeText";
 
 export function HeroSection() {
   return (
-    <div className="h-screen flex flex-col gap-12 justify-center items-center">
+    <div className="h-screen w-full background-image-hero flex flex-col gap-12 justify-center items-center">
       <HeroTextMain></HeroTextMain>
       <LogoTicker></LogoTicker>
     </div>
