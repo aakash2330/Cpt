b59df0f43c288291b559ac20825affbd50397d8a
aakash2330 <aakash2330@gmail.com>
2025-05-21 19:40:24 +0530

    smooth-scroll

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -3,17 +3,20 @@ import { useEffect, useState } from "react";
 import ExperienceSection from "./Components/Sections/Experience";
 import { HeroSection } from "./Components/Sections/Hero";
 import { ProjectsSection } from "./Components/Sections/Projects";
+import ReactLenis from "lenis/react";
 
 export default function Page() {
   const [initialScrollHandled, setInitialScrollHandled] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <HeroSection></HeroSection>
-      <div className="h-[300dvh] w-full  flex flex-col gap-12 justify-center items-center"></div>
-      <ProjectsSection></ProjectsSection>
-      <ExperienceSection></ExperienceSection>
-    </div>
+    <ReactLenis root>
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection></HeroSection>
+        <div className="h-[300dvh] w-full  flex flex-col gap-12 justify-center items-center"></div>
+        <ProjectsSection></ProjectsSection>
+        <ExperienceSection></ExperienceSection>
+      </div>
+    </ReactLenis>
   );
 }
