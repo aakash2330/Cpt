e3d5dcc3b038433568f9f01608ad9a54a7b93561
aakash2330 <aakash2330@gmail.com>
2025-05-21 18:18:36 +0530

    scroll-animation

--- src/app/services/_components/ServiceCard.tsx
+++ src/app/services/_components/ServiceCard.tsx
@@ -1,27 +1,70 @@
+import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
+import { useRef } from "react";
 
 interface ServiceCardProps {
-    image: string;
-    heading: string;
-    description: string;
+  image: string;
+  heading: string;
+  description: string;
 }
 
-export function ServiceCard({ image, heading, description }: ServiceCardProps) {
-    return (
+interface CardProps {
+  i: number;
+  title: string;
+  description: string;
+  url: string;
+  progress: MotionValue<number>;
+  range: [number, number];
+  targetScale: number;
+}
+export function ServiceCard({
+  i,
+  title,
+  description,
+  url,
+  progress,
+  range,
+  targetScale,
+}: CardProps) {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "start start"],
+  });
+
+  const scale = useTransform(progress, range, [1, targetScale]);
+  return (
+    <div
+      ref={container}
+      className="h-screen flex items-center justify-center sticky top-0"
+    >
+      <motion.div
+        style={{
+          scale,
+          top: `calc(-5vh + ${i * 25}px)`,
+        }}
+        className={`flex flex-col relative -top-[25%] h-[450px] w-[100%] rounded-md p-10 origin-top`}
+      >
         <div className="w-full my-6 sm:my-8">
-            <div className="relative w-full h-56 xs:h-64 sm:h-72 md:h-96 lg:h-[400px]">
-                <Image
-                    src={image}
-                    alt="card-image"
-                    fill
-                    className="object-cover rounded-xl sm:rounded-2xl"
-                    loading="lazy"
-                />
-            </div>
-            <div className="text-white flex flex-col gap-2 py-4 sm:flex-row sm:items-center sm:justify-between">
-                <h3 className="text-base xs:text-lg sm:text-xl font-semibold">{heading}</h3>
-                <p className="text-sm xs:text-base sm:text-sm max-w-md">{description}</p>
-            </div>
+          <div className="relative w-full h-56 xs:h-64 sm:h-72 md:h-96 lg:h-[400px]">
+            <Image
+              src={url}
+              alt="card-image"
+              fill
+              className="object-cover rounded-xl sm:rounded-2xl"
+              loading="lazy"
+            />
+          </div>
+          <div className="text-white flex bg-background flex-col gap-2 py-4 sm:flex-row sm:items-center sm:justify-between">
+            <h3 className="text-base xs:text-lg sm:text-xl font-semibold">
+              {title}
+            </h3>
+            <p className="text-sm xs:text-base sm:text-sm max-w-md">
+              {description}
+            </p>
+          </div>
         </div>
-    );
+      </motion.div>
+    </div>
+  );
 }
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,120 +1,120 @@
-'use client'
-import React, { useState, useEffect } from "react";
+"use client";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
-import { useSpring, animated, config } from '@react-spring/web';
+import { useSpring, animated, config } from "@react-spring/web";
 import { ServiceCard } from "./_components/ServiceCard";
+import { useScroll } from "motion/react";
+import ReactLenis from "lenis/react";
+const serviceData = [
+  {
+    image: "/construction_management.jpg",
+    heading: "CONSTRUCTION MANAGEMENT",
+    description:
+      "Overseeing all phases of construction, we coordinate teams and resources to achieve seamless project execution.​​",
+  },
+  {
+    image: "/general_contracting.jpg",
+    heading: "GENERAL CONTRACTING",
+    description:
+      "Managing every aspect of the construction process, we ensure projects are delivered on time, within budget, and to the highest standards.​",
+  },
+  {
+    image: "/residential.jpg",
+    heading: "RESIDENTIAL",
+    description:
+      "Turn your dream home into a reality with our help. We offer comprehensive residential construction services to cater to your every need.",
+  },
+  {
+    image: "/commercial.jpg",
+    heading: "COMMERCIAL",
+    description:
+      "Achieve your ideal look and function for your commercial space with our assistance. We can provide you with exemplary construction solutions to meet your specific needs.",
+  },
+  {
+    image: "/hospitality.jpg",
+    heading: "HOSPITALITY",
+    description:
+      "See your envisioned property design for your hotel or hospitality space come to life with our aid. At City Professional Trades, we offer full-service construction services for this type of real estate.",
+  },
+];
 
 export default function ServicePage() {
-    const [loaded, setLoaded] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
-    const imageAnimation = useSpring({
-        from: { transform: 'translateY(-30px)', opacity: 0 },
-        to: { transform: 'translateY(0px)', opacity: 1 },
-        config: { tension: 170, friction: 26 },
-        delay: 100,
-    });
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
 
-    const textAnimation = useSpring({
-        from: { transform: 'translateY(50px)', opacity: 0 },
-        to: { transform: 'translateY(0px)', opacity: 1 },
-        config: { tension: 170, friction: 22 },
-        delay: 300,
-    });
+  const imageAnimation = useSpring({
+    from: { transform: "translateY(-30px)", opacity: 0 },
+    to: { transform: "translateY(0px)", opacity: 1 },
+    config: { tension: 170, friction: 26 },
+    delay: 100,
+  });
 
-    useEffect(() => {
-        setLoaded(true);
-    }, []);
+  const textAnimation = useSpring({
+    from: { transform: "translateY(50px)", opacity: 0 },
+    to: { transform: "translateY(0px)", opacity: 1 },
+    config: { tension: 170, friction: 22 },
+    delay: 300,
+  });
+  const AnimatedDiv = animated("div");
 
-    const serviceData = [
-        {
-            image: "/construction_management.jpg",
-            heading: "CONSTRUCTION MANAGEMENT",
-            description:
-                "Overseeing all phases of construction, we coordinate teams and resources to achieve seamless project execution.​​",
-        },
-        {
-            image: "/general_contracting.jpg",
-            heading: "GENERAL CONTRACTING",
-            description:
-                "Managing every aspect of the construction process, we ensure projects are delivered on time, within budget, and to the highest standards.​",
-        },
-        {
-            image: "/residential.jpg",
-            heading: "RESIDENTIAL",
-            description:
-                "Turn your dream home into a reality with our help. We offer comprehensive residential construction services to cater to your every need.",
-        },
-        {
-            image: "/commercial.jpg",
-            heading: "COMMERCIAL",
-            description:
-                "Achieve your ideal look and function for your commercial space with our assistance. We can provide you with exemplary construction solutions to meet your specific needs.",
-        },
-        {
-            image: "/hospitality.jpg",
-            heading: "HOSPITALITY",
-            description:
-                "See your envisioned property design for your hotel or hospitality space come to life with our aid. At City Professional Trades, we offer full-service construction services for this type of real estate.",
-        },
-        {
-            image: "/industrial.jpg",
-            heading: "INDUSTRIAL",
-            description:
-                "Build a durable and safe facility for the specific needs of your industrial operations with City Professional Trades. We can provide you with a wide range of building services to help you achieve your construction goals.",
-        },
-        {
-            image: "/custom_construction_solution.jpg",
-            heading: "CUSTOM CONSTRUCTION SOLUTIONS",
-            description:
-                "Tailoring our services to your specific requirements, we deliver bespoke solutions that align with your goals and expectations.",
-        },
-    ];
+  return (
+    <ReactLenis root>
+      <div className="w-full mb-72">
+        <div className="relative h-[60vh] xs:h-[70vh] sm:h-screen w-full overflow-hidden">
+          <AnimatedDiv
+            style={imageAnimation}
+            className="absolute inset-0 bg-black/20"
+          >
+            <Image
+              src="/service_bg.jpg"
+              alt="Construction workers at site"
+              fill
+              className="object-cover -z-1"
+              priority
+            />
+          </AnimatedDiv>
 
-    const AnimatedDiv = animated('div');
-
-    return (
-        <div className="w-full">
-            <div className="relative h-[60vh] xs:h-[70vh] sm:h-screen w-full overflow-hidden">
-                <AnimatedDiv style={imageAnimation} className="absolute inset-0 bg-black/20">
-                    <Image
-                        src="/service_bg.jpg"
-                        alt="Construction workers at site"
-                        fill
-                        className="object-cover -z-1"
-                        priority
-                    />
-                </AnimatedDiv>
-
-                <div className="relative z-10 flex h-full items-end">
-                    <AnimatedDiv
-                        style={textAnimation}
-                        className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 h-auto sm:h-1/3 bg-[#121212] py-8 px-4 xs:px-6 sm:px-12 md:px-16 text-white rounded-tr-3xl"
-                    >
-                        <h2 className="mb-2 text-sm sm:text-base font-semibold tracking-wide">
-                            WHAT WE DO
-                        </h2>
-                        <p className="text-xs xs:text-sm sm:text-base max-w-sm">
-                            At City Professional Trades, we offer a comprehensive range of
-                            construction services tailored to meet the unique needs of each
-                            project. Our expertise spans:
-                        </p>
-                    </AnimatedDiv>
-                </div>
-            </div>
-
-            <div className="px-4 xs:px-6 sm:px-8 md:px-12 lg:px-16 pt-12">
-                <ServiceCard
-                    image="/design_and_build.jpg"
-                    heading="DESIGN AND BUILD"
-                    description="From concept to completion, we provide integrated solutions that bring your vision to life with precision and efficiency.​"
-                />
-            </div>
-
-            {serviceData.map((card, index) => (
-                <div key={index} className="py-6 sm:py-8 px-4 xs:px-6 sm:px-8 md:px-12 lg:px-16">
-                    <ServiceCard {...card} />
-                </div>
-            ))}
+          <div className="relative z-10 flex h-full items-end">
+            <AnimatedDiv
+              style={textAnimation}
+              className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 h-auto sm:h-1/3 bg-[#121212] py-8 px-4 xs:px-6 sm:px-12 md:px-16 text-white rounded-tr-3xl"
+            >
+              <h2 className="mb-2 text-sm sm:text-base font-semibold tracking-wide">
+                WHAT WE DO
+              </h2>
+              <p className="text-xs xs:text-sm sm:text-base max-w-sm">
+                At City Professional Trades, we offer a comprehensive range of
+                construction services tailored to meet the unique needs of each
+                project. Our expertise spans:
+              </p>
+            </AnimatedDiv>
+          </div>
         </div>
-    );
+
+        {serviceData.map((project, i) => {
+          const targetScale = 1 - (serviceData.length - i) * 0.05;
+          return (
+            <ServiceCard
+              key={`p_${i}`}
+              i={i}
+              url={project?.image}
+              title={project?.heading}
+              description={project?.description}
+              progress={scrollYProgress}
+              range={[i * 0.25, 1]}
+              targetScale={targetScale}
+            />
+          );
+        })}
+      </div>
+    </ReactLenis>
+  );
 }
